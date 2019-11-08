7d6ec47f9f898525355703ee62144b8e067c3b61
Alexey Ananyev <hypercodeplace@gmail.com>
2019-11-08 11:35:32 +0500

    Create scaffolding.

---
+++ LazyProxy.Autofac.Tests/AutofacExtensionTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace LazyProxy.Autofac.Tests
+{
+    public class AutofacExtensionTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
